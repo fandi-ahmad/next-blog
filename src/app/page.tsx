d2f1668b9c6e333350ceff5a6e27121a7ba9ae16
fandi-ahmad <fandi4160@gmail.com>
2024-05-24 10:09:03 +0800

    get data article from supabase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,46 @@
+"use client"
 import Card from "@/components/Card";
+import { useEffect, useState } from "react";
+import { GetArticle } from "@/supabase/GetArticle";
+import { DateFormat } from "@/utils/DateFormat";
+
+type dataArticles = {
+  id: number,
+  id_user: number,
+  body_post: string,
+  head_post: string,
+  label: string,
+  thumbnail: any,
+  created_at: string
+}
 
 export default function Home() {
+  const [articleList, setArticleList] = useState<dataArticles[]>([])
+  
+  const getAllArticle = async () => {
+    const response = await GetArticle()
+    if (response.data) {
+      setArticleList(response.data);
+    }
+  }
+
+  useEffect(() => {
+    getAllArticle()
+  }, [])
+
   return (
     <main>
       <div className="mt-4">
-        <Card/>
-        <Card/>
-        <Card/>
+        {articleList.map((article) => (
+          <Card
+            key={article.id}
+            head={article.head_post}
+            body={article.body_post}
+            created_at={DateFormat(article.created_at)}
+            label={article.label}
+            src={article.thumbnail}
+          />
+        ))}
       </div>
     </main>
   );
