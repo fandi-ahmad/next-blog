c70fa9ac89fae0cbb3cfc156bf8aae91d4a35d6b
fandi-ahmad <fandi4160@gmail.com>
2024-06-10 00:07:53 +0800

    add register

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -2,8 +2,36 @@ import { TextField } from "@mui/material";
 import { Heading } from "../(components)/Heading";
 import { SubmitButton } from "../(components)/SubmitButton";
 import Link from "next/link";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { createClient } from "@/lib/supabase/server";
 
 export default function Register() {
+  const signUp = async (formData: FormData) => {
+    "use server";
+
+    const origin = headers().get("origin");
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    const supabase = createClient();
+
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `${origin}/auth/callback`,
+      },
+    });
+
+    console.log(error, '<-- ERROR REGSTER');
+
+    if (error) {
+      return redirect("/register?message=Could not authenticate user");
+    }
+
+    return redirect("/register?message=Check email to continue sign in process");
+  };
+
   return (
     <>
       <Heading>Sign Up</Heading>
@@ -11,7 +39,7 @@ export default function Register() {
         <TextField label="Email" name="email" variant="outlined" type="email" required className="w-full mb-4" />
         <TextField label="Password" name="password" variant="outlined" type="password" required className="w-full mb-6" />
         
-        <SubmitButton pendingText="Signing Up..." className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2 w-full text-white">
+        <SubmitButton formAction={signUp} pendingText="Signing Up..." className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2 w-full text-white">
           Sign Up
         </SubmitButton>
         
